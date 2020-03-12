4d14fedcd2e3542fe1a284cfc4ce36b6634a0733
Jorelle Agustin <jagustin@stratpoint.com>
2020-03-12 09:50:40 +0800

    change findByPk to _getById

--- auth.js
+++ auth.js
@@ -10,11 +10,8 @@ module.exports = (config, userModel) => {
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt')
   }
 
-  const test = userModel.findByPk('6ba3b41e-13a7-4e25-b6aa-ce855d1a2983')
-  console.log(test)
-
   const strategy = new Strategy(params, (payload, done) => {
-    userModel.findByPk(payload.id)
+    userModel._getById(payload.id)
       .then((user) => {
         done(null, user)
       })
