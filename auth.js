d66f53bbd838cad068a0264074d7f6c85bf33600
Jorelle Agustin <jagustin@stratpoint.com>
2020-03-13 20:54:26 +0800

    changed to getById

--- auth.js
+++ auth.js
@@ -4,14 +4,14 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-module.exports = (config, userModel) => {
+module.exports = (config, UserRepository) => {
   let opts = {};
    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('JWT');
    opts.secretOrKey = config.secret;
 
 
   const strategy = new JwtStrategy(opts, (payload, done) => {
-    userModel._getById(payload.id)
+    UserRepository.getById(payload.id)
       .then((user) => {
         done(null, user)
       })
