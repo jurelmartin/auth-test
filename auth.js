97780bd322b402953e44e1a2c773d1ff66065e87
Jorelle Agustin <jagustin@stratpoint.com>
2020-03-15 20:51:50 +0800

    working auth

--- auth.js
+++ auth.js
@@ -1,33 +1,40 @@
-
-
 const passport = require('passport');
-const { ExtractJwt, Strategy } = require('passport-jwt');
+const JwtStrategy = require('passport-jwt').Strategy;
+
 
 module.exports = (config, UserRepository) => {
-  const opts = {
+
+  const strategyOptions = {
     secretOrKey : config.secret,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    jwtFromRequest: req => req.headers.cookie,
+    passReqToCallback: true
   };
 
-  console.log(ExtractJwt.fromAuthHeaderAsBearerToken())
-  const strategy = new Strategy(opts, (payload, done) => {
-    
-    console.log(payload)
-    UserRepository.getById(payload.id)
-      .then((user) => {
-        done(null, user)
-      })
-      .catch((error) => done(error, null))
-  })
+  const verifyCallback = async (req, jwtPayload, cb) => {
+    const [err, user] = await UserRepository.getById(jwtPayload.dataValues.id)
+    console.log(user);
+    if(err) {
+      return cb(err)
+    }
+    req.user = user
+    return cb(null, user)
+  }
 
-  passport.use(strategy)
- 
-  passport.serializeUser(function (user, done) {
-    done(null, user)
-  })
+  passport.use(new JwtStrategy(strategyOptions, verifyCallback))
 
-  passport.deserializeUser(function (user, done) {
-    done(null, user)
+
+
+  passport.serializeUser((user, done) => done (null, user.dataValues.id))
+
+
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await UserRepository.getById(id)
+      return done(null, user)
+    }catch (err) {
+      return done(err, null)
+    }
   })
 
   return {
@@ -40,19 +47,3 @@ module.exports = (config, UserRepository) => {
   }
 
 }
-// module.exports = () => {
-//   return {
-//     authenticate: () => {
-//       return passport.authenticate('jwt')
-//     }
-//   }
-// }
-
-// module.exports = () => {
-//   return {
-//     initialize: () => {
-//       return passport.initialize()
-//     }
-//   }
-
-// }
